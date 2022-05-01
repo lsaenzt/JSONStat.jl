b6c34ad3497e87bf8c3c99e25ae65e8bc9c17117
lsaenzt <59137624+lsaenzt@users.noreply.github.com>
2022-05-01 18:31:05 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,10 @@ using Test, Downloads
 using JSONStat
 
 io = IOBuffer()
+
 resp = Downloads.download("https://json-stat.org/samples/canada.json", io) |> take!
-
 @test JSONStat.name(JSONStat.read(resp)) == "Population by sex and age group. Canada. 2012"
-
 
 resp = Downloads.download("https://json-stat.org/samples/galicia.json", io) |> take!
 @test JSONStat.Tables.columnnames(galicia) |> length == 7
 
-resp = Downloads.download("https://json-stat.org/samples/us-gsp.json", io) |> take!
